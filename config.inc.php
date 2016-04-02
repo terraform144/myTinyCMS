d991fe6922da2ce39b6b7f81e413ec2213a9dc3c
zulad <jl_beuzard@hotmail.com>
2016-04-02 21:45:43 +0200

    add

---
+++ config.inc.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * 	configure my tiny CMS
+ */
+
+define("ROOT","") ;
+
+define("CMS"	, ROOT."Cms") ;
+define("OWN"	, ROOT."Owners") ;
+
+define("CLA"	,CMS . "/Classes") ;
+define("FUN"	,CMS . "/Functions") ;
+
+// 
+define("OBJ"	,CMS . "/Objects") ;
+
+	define("BCK"	,OBJ . "/Backend") ;
+	define("FNT"	,OBJ . "/Frontend") ;
+
+?>
