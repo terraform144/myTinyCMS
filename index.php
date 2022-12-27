b3d2436f7cc77c7ca02e5e53707a58068cde455e
zululad <gianluigi@terraform144.com>
2022-12-27 21:43:56 +0000

    route fix !

--- index.php
+++ index.php
@@ -11,34 +11,37 @@ require "vendor/autoload.php";
 $routes = [
   "/" => "home.php",
   "/about" => "about.php",
-  "/contact" => "contact.php",
-  "/smoothie" => "Smoothie_maker_portable"
+  "/contact" => "contact.php"
+];
+$prd_routes = [
+  "/smoothie-maker" => "Smoothie_maker_portable"
 ];
 
 $currentUrl = parse_url($_SERVER["REQUEST_URI"]);
 
 // todo adapter la profondeur de route !!
 // Parcourez les routes et vérifiez si l'URL courante correspond à l'une d'entre elles
-foreach ($routes as $pattern => $view) {
+/**foreach ($routes as $pattern => $view) {
   if (preg_match("#^" . $pattern . "$#", $currentUrl, $matches)) {
     // Si oui, incluez le fichier de vue associé à la route
     include __DIR__ . "/views/" . $view;
     // Arrêtez la boucle foreach
     break;
   }
-}
+} // Todo completion -> Ask chatGPT*/
 
-if (!isset($routes[$currentUrl['path']])) {
-  http_response_code(404);
-  echo "Page non trouvée";
+if (isset($routes[$currentUrl['path']])) {
+  require_once ($routes[$currentUrl['path']]);  die();
 } else {
-  require_once ($routes[$currentUrl['path']]);
-  die();
+  if ( ! array_key_exists($currentUrl['path'], $prd_routes)) {
+    http_response_code(404);
+    echo "Page non trouvée"; die();
+  }
 }
 
 // todo change entry prd field
 ##region ENTRY OF PRODUCT
-@$prd_page = $routes[$currentUrl]; //'Smoothie_maker_portable'; //$_REQUEST['prd'];
+@$prd_page = $prd_routes[$currentUrl['path']]; //'Smoothie_maker_portable'; //$_REQUEST['prd'];
 ##endregion ENTRY OF PRODUCT
 
 # INC /** Here comes all the inclusions */
