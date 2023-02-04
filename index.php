cb12632b4f608027c014397905d7185f0c3281c6
zululad <gianluigi@terraform144.com>
2023-02-04 14:32:50 +0000

    !

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $routes = [ // here comes all the common routes for the application non prd
   "/" => "home.php",
   "/about" => "about.php",
   "/contact" => "contact.php",
-  "/euromut" => "base/cr/index.php"
+  "/dark-basic" => "base/dark_basic/index.php"
 ];
 $prd_routes = [ // here comes all the prd funnels 
   "/smoothie-maker" => "Smoothie_maker_portable"
